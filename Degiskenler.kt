76038ee33cd58fa460b12a4f9df7d22b7dd806b5
Samethannkul <samethannkul@gmail.com>
2025-04-26 15:05:34 +0300

    Add files via upload

---
+++ Degiskenler.kt
@@ -0,0 +1,97 @@
+package com.example.kotlinders1.odevler
+
+fun main() {
+    // Degiskenleri tanimliyoruz
+    var sehir = "Istanbul"
+    var ulke = "Turkiye"
+    var telefon = "555-123-4567"
+    var postaKodu = "34000"
+    var email = "ahmet@example.com"
+    var meslek = "Muhendis"
+    var stokMiktari = 100
+    var musteriAdi = "Ahmet Yilmaz"
+    var bakiye = 1500.50
+    var dogumGunu = "01.01.1990"
+    var maas = 7500.0
+    var medeniDurum = "Evli"
+    var urunYorum = "Cok iyi bir urun"
+    var odemeTarihi = "24.04.2025"
+    var siparisAdeti = 5
+    var arabaModeli = "Toyota Corolla"
+    var kitapAdi = "Kotlin Programlama"
+    var yayinlamaTarihi = "2023"
+    var indirimMiktari = 10.0
+    var odaSayisi = 3
+    var enlem = 41.0082
+    var boylam = 28.9784
+    var urunAdi = "Telefon"
+    var yemekFiyati = 50.0
+    var marka = "Samsung"
+    var muzikAdi = "Klasik Muzik"
+    var videoSuresi = "2:30"
+    var urunPuani = 8
+    var resimAdi = "manzara.jpg"
+    var dosyaFormati = "PDF"
+    var renk = "Mavi"
+    var renkKodu = "#FF0000"
+    var telefonModeli = "Galaxy S21"
+    var ekranBoyutu = "15 inc"
+    var ulusalGun = "29 Ekim"
+    var tatilGunu = "1 Mayis"
+    var rezervasyonTarihi = "24.04.2025"
+    var sokakAdi = "Cicek Sokak"
+    var otobusHatti = "34A"
+    var kalanDakika = 15
+    var takipKodu = "TR123456"
+    var kuponSuresi = "30 gun"
+    var kuponKodu = "INDIRIM50"
+    var faturaAdresi = "Cicek Sokak No: 5, Istanbul"
+    var agirlik = "2 kg"
+
+    // Degiskenleri ekrana yazdiriyoruz
+    println("Sehir: $sehir")
+    println("Ulke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktari: $stokMiktari")
+    println("Musteri Adi: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Dogum Gunu: $dogumGunu")
+    println("Maas: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Urun Yorum: $urunYorum")
+    println("Odeme Tarihi: $odemeTarihi")
+    println("Siparis Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adi: $kitapAdi")
+    println("Yayinlama Tarihi: $yayinlamaTarihi")
+    println("Indirim Miktari: $indirimMiktari")
+    println("Oda Sayisi: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Urun Adi: $urunAdi")
+    println("Yemek Fiyati: $yemekFiyati")
+    println("Marka: $marka")
+    println("Muzik Adi: $muzikAdi")
+    println("Video Suresi: $videoSuresi")
+    println("Urun Puani: $urunPuani")
+    println("Resim Adi: $resimAdi")
+    println("Dosya Formati: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ulusal Gun: $ulusalGun")
+    println("Tatil Gunu: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adi: $sokakAdi")
+    println("Otobus Hatti: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Suresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+    println("Agirlik: $agirlik")
+}
